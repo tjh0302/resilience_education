c5d3cb3cc65b2b95edaf62c55dd133ea1a4d3a4b
hparten <hallieparten@gmail.com>
2024-07-30 11:00:05 -0500

    commented dev notebooks

--- ragsilience/setup_rag.py
+++ ragsilience/setup_rag.py
@@ -114,7 +114,7 @@ def create_rag_chain(persist_directory):
     contextualize_q_system_prompt = '''Given a chat history and the latest user question 
     which might reference context in the chat history, formulate a standalone question which can 
     be understood without a chat history. Do NOT answer the question, just reformulate it if needed 
-    and otherwise return it as is. Do not stream this new question'''
+    and otherwise return it as is.'''
 
     contextualize_q_prompt = ChatPromptTemplate.from_messages(
         [
